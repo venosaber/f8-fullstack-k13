cea229363e23ab250359af008215370f72d12f6f
Trung Nguyen <trung.nguyenduc.dev@gmail.com>
2025-03-23 10:58:15 +0700

    Nop bai tap lan 1

---
+++ Day-18/timeout.js
@@ -0,0 +1,54 @@
+const cart = [
+    { id: 1, name: "Laptop", price: 1500 },
+    { id: 2, name: "Phone", price: 800 },
+    { id: 3, name: "Tablet", price: 500 }
+];
+
+/*
+ ┌──────────────┐
+ │  cart        │
+ │              │
+ │  [           │
+ │    {         │                         ┌───────────────┐
+ │     id:      │      findIndex()        │               │  false
+ │              │ ─────────────────────►  │  index !==-1 ?│────────────► print "not exist"
+ │     name:    │                         └───────────────┘
+ │     price:   │ productId === product.id       │
+ │    }         │                                │
+ │   ...        │                                │
+ │              │                                │ true
+ │  ]           │                                │
+ └──────────────┘                                │
+                                                 ▼
+
+                                       setTimeout(function, delay)
+                                                     │
+                                                     │
+                                                     │
+                                                     │
+                                                     ▼
+                                             splice(index,1) (remove)
+
+                                             print the cart
+
+ */
+
+function removeItemAfterDelay(productId, delay){
+    const index = getIndex(productId);
+    if(index!==-1){
+        setTimeout(()=>{
+            cart.splice(index,1);
+            console.log(`The cart after removing the product (which id is ${productId}):`);
+            console.log(cart);
+        }, delay);
+    }else{
+        console.log("No such product exists");
+    }
+}
+
+function getIndex(productId){
+    return cart.findIndex(product=>product.id === productId);
+}
+
+// for example
+removeItemAfterDelay(2, 2000);
