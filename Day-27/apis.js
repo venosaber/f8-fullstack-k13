182c65d609acfb435a05a37c2fa3d8daf92bf537
Trung Nguyen <trung.nguyenduc.dev@gmail.com>
2025-04-16 07:25:11 +0700

    nop bai lan 1

--- Day-27/apis.js
+++ Day-27/apis.js
@@ -41,10 +41,10 @@ const baseUrl = 'https://8fty49z8qb.execute-api.ap-southeast-1.amazonaws.com';
 
 
  */
-async function onSignIn(email, password){
+async function onSignIn(email, password) {
     const payload = {email, password};
-    try{
-        const response = await fetch(`${baseUrl}/login`,{
+    try {
+        const response = await fetch(`${baseUrl}/login`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -52,7 +52,7 @@ async function onSignIn(email, password){
             body: JSON.stringify(payload)
         });
 
-        if(!response.ok){
+        if (!response.ok) {
             throw new Error("Sign-in failed!")
         }
         const {access, refresh} = await response.json();
@@ -60,8 +60,8 @@ async function onSignIn(email, password){
         console.log("Sign-in successfully! Tokens have been saved!")
         // Redirect to post page
         window.location.href = "post.html";
-    }catch (error){
-        console.error("Error: ",error);
+    } catch (error) {
+        console.error("Error: ", error);
         alert("Wrong email or password!")
     }
 }
@@ -93,7 +93,7 @@ async function onSignIn(email, password){
 
 
  */
-async function getNewAccessToken(){
+async function getNewAccessToken() {
     const {refresh} = loadTokens();
     try {
         const response = await fetch(`${baseUrl}/login/get_new_token/`, {
@@ -106,13 +106,13 @@ async function getNewAccessToken(){
 
         if (!response.ok) {
             throw new Error("Error while refreshing token");
-        }else{
+        } else {
             const {access, refresh} = await response.json();
             saveTokens(access, refresh);
             console.log("Refresh tokens successfully!")
         }
-    }catch (error){
-        console.error("Error: ",error);
+    } catch (error) {
+        console.error("Error: ", error);
         localStorage.removeItem("access");
         localStorage.removeItem("refresh");
         window.location.href = "index.html"; // redirect to sign in form
@@ -154,9 +154,9 @@ async function getNewAccessToken(){
 └──────────────────────────────────────────┘
 
  */
-async function getPostContent(rootE){
+async function getPostContent(rootE) {
     let {access} = loadTokens();
-    if(!access){
+    if (!access) {
         window.location.href = "index.html";
         return; // force to stop execution
     }
@@ -171,13 +171,13 @@ async function getPostContent(rootE){
         });
 
         // handle 401 error (Unauthorized)
-        if(response.status === 401){
-            console.log("Access token expired or invalid. Refreshing ...");
-            try{
+        if (response.status === 401) {
+            console.log("Access token is expired or invalid. Refreshing ...");
+            try {
                 await getNewAccessToken();
                 await getPostContent(rootE); // try to get the post with new access token
                 return;
-            }catch (refreshError){
+            } catch (refreshError) {
                 // if failed in refreshing => getNewAccessToken will redirect
                 console.error("Failed to refresh token: ", refreshError);
                 rootE.textContent = "Session expired. Please log in again.";
@@ -186,11 +186,11 @@ async function getPostContent(rootE){
         }
 
         // handle other exceptions
-        if(!response.ok){
+        if (!response.ok) {
             let errorData;
-            try{
+            try {
                 errorData = await response.json();
-            }catch(e){
+            } catch (e) {
                 // do nothing if we cannot read json
             }
             console.error("API Error Status:", response.status, "Data:", errorData);
@@ -201,18 +201,18 @@ async function getPostContent(rootE){
         const data = await response.json();
         rootE.textContent = JSON.stringify(data);
 
-    }catch (error){
-        console.error("Error fetching post content: ",error);
+    } catch (error) {
+        console.error("Error fetching post content: ", error);
         rootE.textContent = "Failed to fetch post."
     }
 }
 
-function saveTokens(access, refresh){
+function saveTokens(access, refresh) {
     localStorage.setItem("access", access);
     localStorage.setItem("refresh", refresh);
 }
 
-function loadTokens(){
+function loadTokens() {
     const access = localStorage.getItem("access");
     const refresh = localStorage.getItem("refresh");
     return {access, refresh};
