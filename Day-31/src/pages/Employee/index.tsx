f3a74c1f109df26c336961d9428107fd8d6f8f4f
Trung Nguyen <trung.nguyenduc.dev@gmail.com>
2025-04-25 08:06:43 +0700

    nop bai lan 1

--- Day-31/src/pages/Employee/index.tsx
+++ Day-31/src/pages/Employee/index.tsx
@@ -1,21 +1,21 @@
-import {Header, Employee} from "../../utils";
-import FTable from "../../components/FTable";
+// import {Header, Employee} from "../../utils";
+// import FTable from "../../components/FTable";
 
-const headers: Header[] = [
-  {name: 'id', text: 'ID'},
-  {name: 'name', text: 'Ten'},
-  {name: 'age', text: 'Tuoi'},
-  {name: 'address', text: 'Dia Chi'},
-  {name: 'action', text: ''}
-];
+// const headers: Header[] = [
+//   {name: 'id', text: 'ID'},
+//   {name: 'name', text: 'Ten'},
+//   {name: 'age', text: 'Tuoi'},
+//   {name: 'address', text: 'Dia Chi'},
+//   {name: 'action', text: ''}
+// ];
 
 export default () => {
-  const employees: Employee[] = [
-    {id: 1, name: 'Dung', age: 20, address: 'Thanh Oai - Ha Noi'},
-    {id: 2, name: 'Trung', age: 22, address: 'Quoc Oai - Ha Noi'},
-    {id: 3, name: 'Son', age: 221, address: 'Thanh Oai - Ha Noi'},
-    {id: 4, name: 'Anh', age: 25, address: 'Thanh Oai - Ha Noi'},
-  ]
+  // const employees: Employee[] = [
+  //   {id: 1, name: 'Dung', age: 20, address: 'Thanh Oai - Ha Noi'},
+  //   {id: 2, name: 'Trung', age: 22, address: 'Quoc Oai - Ha Noi'},
+  //   {id: 3, name: 'Son', age: 221, address: 'Thanh Oai - Ha Noi'},
+  //   {id: 4, name: 'Anh', age: 25, address: 'Thanh Oai - Ha Noi'},
+  // ]
 
   return (
     <>
