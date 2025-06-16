29e9b2b68774741be669ca3e0a1842c23d647058
venosaber <trung.nguyenduc.dev@gmail.com>
2025-06-16 12:19:37 +0700

    Delete Day-36 directory

--- Day-36/api_v1/src/color/entity.ts
+++
@@ -1,8 +0,0 @@
-import {Entity, Column, PrimaryGeneratedColumn, Table} from 'typeorm';
-import { BaseEntity } from '../base/entity'
-
-@Entity('color')
-export class ColorEntity extends BaseEntity {
-  @Column()
-  name: string;
-}
--- Day-36/web/src/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './type/common.ts'
-export * from './type/dialog.ts'
-export * from './api.ts'
