a453191b7075e0141ed12a21af940d82766085dd
Trung Nguyen <trung.nguyenduc.dev@gmail.com>
2025-07-28 19:25:34 +0700

    show info

--- Day-58/api/src/modules/image/controller.ts
+++ Day-58/api/src/modules/image/controller.ts
@@ -46,7 +46,7 @@ export class ImageController {
       // use Pipe to validate the file
       new ParseFilePipe({
         validators: [
-          new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 5 }), // 5MB
+          new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 10 }), // 10MB
           new FileTypeValidator({ fileType: '.(png|jpeg|jpg|gif)' }),
         ],
       }),
